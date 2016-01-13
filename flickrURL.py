04d4b540c69f50714319cd7dcef79a8346237a86
I-Chiao Hsu <ichiao.hsu@gmail.com>
2016-01-13 23:42:28 +0100

    get photoset list failed

--- flickrURL.py
+++ flickrURL.py
@@ -2,7 +2,7 @@ import urllib
 import oauth
 import hidden
 import re
-
+import json
 #request_token_url = "http://www.flickr.com/services/oauth/request_token"
 authorize_url = "http://www.flickr.com/services/oauth/authorize"
 access_token_url = "http://www.flickr.com/services/oauth/access_token"
@@ -18,9 +18,7 @@ def init_secrets():
 def get_user(url):
 	
 	secrets = init_secrets()
-	#secrets["oauth_timestamp"] = oauth.generate_timestamp()
-	#secrets["oauth_nonce"] = oauth.generate_nonce()
-	init_secrets()
+
 	# Setup the consumer with api_key and api_secret
 	consumer = oauth.OAuthConsumer(secrets["oauth_consumer_key"], secrets["oauth_consumer_secret"])
 	# Create request
@@ -55,7 +53,8 @@ def get_user(url):
 	secrets["oauth_token"] = request_token["oauth_token"]
 	secrets["oauth_verifier"] = oauth_verifier
 
-	#print secrets
+	print secrets
+	print
 
 	del secrets["oauth_consumer_secret"]
 
@@ -66,12 +65,47 @@ def get_user(url):
 	connection = urllib.urlopen(url)
 	data = connection.read()
 
-	#print "data: ", data
+	print "data: ", data
+	print
 
+	secrets["oauth_token"] = re.findall("oauth_token=(.+?)&", data)[0]
+	secrets["oauth_token_secret"] = re.findall("oauth_token_secret=(.+?)&", data)[0]
+	secrets["username"] = re.findall("username=(.+)",data)[0]
+	secrets["user_nsid"] = re.findall("user_nsid=(.+?)&",data)[0]
+	"""
 	user = {
-		"user_nsid": re.findall("user_nsid=(.+)&",data)[0],
-		"username": re.findall("username=(.+)",data)[0]
-	}
+		"user_nsid": re.findall("user_nsid=(.+?)&",data)[0],
+		"username": re.findall("username=(.+)",data)[0],
+		"oauth_token": re.findall("oauth_token=(.+?)&", data)[0],
+		"oauth_token_secret": re.findall("oauth_token_secret=(.+?)&", data)[0]
+	}"""
 
 	#print user
-	return user
+	return secrets
+
+
+def get_Photosets_list(api_url, params):
+	
+
+	urlprms = {
+		"method": "flickr.photosets.getList",
+		"api_key": params["oauth_consumer_key"],
+		"user_id": params["user_nsid"],
+		"format": "json",
+		"nojsoncallback": "1",
+		"auth_token": params["oauth_token"],
+		"api_sig": params["oauth_signature"]
+	}
+
+	url = api_url + urllib.urlencode(urlprms)
+	print "photosets url: ", url
+	print
+
+	data = urllib.urlopen(url).read()
+
+	print data
+	print
+
+	js = json.loads(data)
+
+	print js
--- get.py
+++ get.py
@@ -1,6 +1,9 @@
-from flickrURL import get_user
+import flickrURL
 
 request_token_url = "http://www.flickr.com/services/oauth/request_token"
+api_url = "https://api.flickr.com/services/rest/?"
 
-user_info = get_user(request_token_url)
+user_info = flickrURL.get_user(request_token_url)
 print user_info
+
+flickrURL.get_Photosets_list(api_url, user_info)
