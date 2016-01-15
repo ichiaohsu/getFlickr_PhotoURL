d3996635199232ddd049f3afc94d58da00efa135
I-Chiao Hsu <ichiao.hsu@gmail.com>
2016-01-15 17:51:04 +0100

    get.py redundant clear

--- get.py
+++ get.py
@@ -1,16 +1,12 @@
 import flickr
 
-request_token_url = "http://www.flickr.com/services/oauth/request_token"
-api_url = "https://api.flickr.com/services/rest/?"
+#request_token_url = "http://www.flickr.com/services/oauth/request_token"
 
 getflickr = flickr.photosets()
 
 #fk = flickr.flickrInit()
 #fk.get_Tokens(request_token_url)
 user_id = raw_input("Enter user id - ")
-
-#url = getflickr.get_photoSize_URL_photoid(23608652504,800)
-#print url
 
 photoset_list = getflickr.get_photoset_List(user_id,1,2)
 
