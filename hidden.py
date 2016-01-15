c7b1e81847dd35c9dcf4a3266fc5010bf3551d75
I-Chiao Hsu <ichiao.hsu@gmail.com>
2016-01-15 17:48:40 +0100

    upload version

---
+++ hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+# Put api key in oauth_consumer_key
+# And secret under oauth_consumer_secret
+# do include this file in flickr.py
+
+def keys():
+	return {
+		"oauth_consumer_key" : "...",
+        "oauth_consumer_secret" : "...",
+	}
