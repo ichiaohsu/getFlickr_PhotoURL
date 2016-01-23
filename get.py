7d12edff4656394d78b7cd8e5870dad00aef7f05
I-Chiao Hsu <ichiao.hsu@gmail.com>
2016-01-24 01:01:33 +0800

    output js file

--- get.py
+++ get.py
@@ -1,4 +1,5 @@
 import flickr
+import json
 
 getflickr = flickr.photosets()
 
@@ -9,7 +10,7 @@ user_id = raw_input("Enter user id - ")
 
 photoset_list = getflickr.get_photoset_List(user_id,1,2)
 
-photo_list = getflickr.get_photolist_from_setid(user_id,photoset_list[0]["id"])
+photo_list = getflickr.get_photolist_from_setid(user_id,photoset_list[1]["id"])
 
 print photo_list
 
@@ -19,3 +20,8 @@ for i in photo_list["photo"]:
 	i["src"] = src
 
 print photo_list
+
+fp = open("Album.js","w")
+json.dump(photo_list, fp, indent=4)
+
+fp.close()
